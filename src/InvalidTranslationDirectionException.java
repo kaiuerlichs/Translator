6d2318ea95fdaef843a0328c92e61f7872c91aba
kaiuerlichs <2421101@dundee.ac.uk>
2021-03-28 17:36:12 +0100

    Add serialVersionUIDs

--- src/InvalidTranslationDirectionException.java
+++ src/InvalidTranslationDirectionException.java
@@ -1,2 +1,8 @@
+import java.io.Serial;
+
 public class InvalidTranslationDirectionException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 4864703291935233901L;
+
 }

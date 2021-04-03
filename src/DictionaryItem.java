1235ec1c774455f1ed292dd3791775445c23ec20
kaiuerlichs <2421101@dundee.ac.uk>
2021-04-03 22:33:16 +0100

    Add some comments here and there

--- src/DictionaryItem.java
+++ src/DictionaryItem.java
@@ -10,19 +10,37 @@ public class DictionaryItem implements Comparable<DictionaryItem>, Serializable 
     private String original;
     private String translation;
 
+    /**
+     * Default constructor for a DictionaryItem
+     * @param pOriginal The original word
+     * @param pTranslation The translated word
+     */
     public DictionaryItem(String pOriginal, String pTranslation){
         original = pOriginal;
         translation = pTranslation;
     }
 
+    /**
+     * Returns the original word
+     * @return The original word
+     */
     public String getOriginal(){
         return original;
     }
 
+    /**
+     * Returns the translated word
+     * @return The translated word
+     */
     public String getTranslation(){
         return translation;
     }
 
+    /**
+     * Compare the DictionaryItem to another
+     * @param x the Item to compare to
+     * @return the comparison value
+     */
     @Override
     public int compareTo(DictionaryItem x) {
         return original.toLowerCase().compareTo(x.getOriginal().toLowerCase());

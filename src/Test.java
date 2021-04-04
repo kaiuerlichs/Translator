9691ae4e566aa504f1d2ae1760c106f2cabf5509
chrisomay <2418120@dundee.ac.uk>
2021-04-04 15:46:41 +0100

    tester

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,3 @@
-import java.io.File;
-import java.time.Duration;
-import java.time.Instant;
-
 public class Test {
 
     /**
@@ -10,8 +6,27 @@ public class Test {
      */
     public static void main(String[] args) {
 
+        Dictionary d = new Dictionary("Test", "a", "b");
 
+        //adding items to dictionary test
+        boolean result = d.add("a"  , "b");
+        boolean result2 = d.add ("c", "d");
+        boolean result3 = d.add ("a","b");
+
+        System.out.println("Adding item successful: " + result);
+        System.out.println("Adding item successful: " + result2);
+        System.out.println("Adding item successful: " + result3);
+
+        //boolean findResult = d.find("a");
+
+        //removing item from dictionary test
+        boolean removeResult = d.remove ("a");
+        boolean removeResult2 = d.remove ("a");
+
+        System.out.println("Removing item successful: " + removeResult);
+        System.out.println("Removing item successful: " + removeResult2);
 
     }
+
 
 }
